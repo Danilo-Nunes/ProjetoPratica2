97e2ee416c79c01c213ab7710248ffcc2b3a9329
Danilo-Nunes <42344266+Danilo-Nunes@users.noreply.github.com>
2019-04-12 11:15:38 -0300

    rice

--- SQL/Banco.sql
+++ SQL/Banco.sql
@@ -1,5 +1,5 @@
 create table Aluno(
-codigo int primary key indentity(1,1) not null,
+codigo int primary key identity(1,1) not null,
 ra char(5) not null,--?
 nome varchar(50) not null,
 nomeUsu varchar(50) not null,
@@ -8,15 +8,15 @@ email varchar(50) not null
 )
 
 create table Professor(
-codigo int primary key indentity(1,1) not null,
-nome varchar(50) not null
+codigo int primary key identity(1,1) not null,
+nome varchar(50) not null,
 nomeUsu varchar(50) not null,
 senha varchar(50) not null,
 email varchar(50) not null
 )
 
 create table Sala(
-codigo int primary key indentity(1,1) not null,
+codigo int primary key identity(1,1) not null,
 nome varchar(50) not null,
 codProfessor int not null,
 constraint fkcodProf foreign key(codProfessor) references Professor(codigo)
@@ -32,7 +32,7 @@ constraint fkSala foreign key(codSala) references Sala(codigo)
 )
 
 create table Atividade(
-codigo int primary key indentity(1,1) not null,
+codigo int primary key identity(1,1) not null,
 numero int not null,
 codSala int not null,
 dataAtividade datetime not null,
@@ -50,7 +50,7 @@ constraint fkAtividade foreign key(codAtividade) references Atividade(codigo)
 )
 
 create table Comunicado(
-codComunicado int primary key indentity(1,1) not null,
+codComunicado int primary key identity(1,1) not null,
 codSala int not null,
 texto varchar(500) not null,
 assunto varchar(50) not null,
@@ -58,7 +58,7 @@ constraint fkSala foreign key (codSala) references Sala(codigo)
 )
 
 create table Compromissos(
-codCompromisso int primary key indentity(1,1) not null,
+codCompromisso int primary key identity(1,1) not null,
 texto varchar(500) not null,
 dataComp datetime not null,
 codAluno int not null,
@@ -68,12 +68,12 @@ constraint fkAlunoComp foreign key (codAluno) references Aluno(codigo)
 create proc SalaAlunos
 @codSala int
 as
-select * from Aluno where codAluno in (select codAluno from AlunoSala where codSala = @codSala)
+select * from Aluno where codigo in (select codAluno from AlunoSala where codSala = @codSala)
 
 create proc SalasAluno
 @codAluno int
 as
-select * from Sala where codSala in (select codSala from AlunoSala where codAluno = @codAluno)
+select * from Sala where codigo in (select codSala from AlunoSala where codAluno = @codAluno)
 
 create proc ComunicadoAluno
 @codAluno int
